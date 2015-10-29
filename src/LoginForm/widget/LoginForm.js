fddd26f60c9aa955122c9bb1c7f429b355ef8cdc
Chad Evans <chad.evans@mendix.com>
2015-10-29 08:26:06 -0400

    Adding custom login microflow

--- src/LoginForm/widget/LoginForm.js
+++ src/LoginForm/widget/LoginForm.js
@@ -17,7 +17,7 @@
 
 */
 
-(function() {
+(function () {
     'use strict';
 
     // test
@@ -33,7 +33,7 @@
         dojo.provide('LoginForm.widget.LoginForm');
 
         // Declare widget.
-        return declare('LoginForm.widget.LoginForm', [ _WidgetBase, _Widget, _Templated ], {
+        return declare('LoginForm.widget.LoginForm', [_WidgetBase, _Widget, _Templated], {
 
             /**
              * Internal variables.
@@ -42,13 +42,13 @@
             _handle: null,
 
             // Extra variables
-            _userInput : null,
-            _passInput : null,
-            _captionShow : null,
-            _captionHide : null,
+            _userInput: null,
+            _passInput: null,
+            _captionShow: null,
+            _captionHide: null,
 
-            _indicator : null,
-            _i18nmap : null,
+            _indicator: null,
+            _i18nmap: null,
 
             // Template path
             templatePath: require.toUrl('LoginForm/widget/templates/LoginForm.html'),
@@ -71,7 +71,7 @@
 
             // DOJO.WidgetBase -> Startup is fired after the properties of the widget are set.
             startup: function () {
-                
+
             },
 
             /**
@@ -79,7 +79,7 @@
              */
 
             update: function (obj, callback) {
-                
+
                 // Execute callback.
                 if (typeof callback !== 'undefined') {
                     callback();
@@ -87,7 +87,7 @@
             },
 
             unintialize: function () {
-                
+
             },
 
 
@@ -98,47 +98,47 @@
             _setupWidget: function () {
                 var templateWithView = null,
                     templateWithoutView = null;
-                
-                if (this.showImage){
+
+                if (this.showImage) {
                     this._captionShow = '<img src="' + this.showImage + '" id="' + this.id + '_image" />';
                 } else {
                     this._captionShow = '';
                 }
-                if (this.hideImage){
+                if (this.hideImage) {
                     this._captionHide = '<img src="' + this.hideImage + '" id="' + this.id + '_image" />';
                 } else {
                     this._captionHide = '';
                 }
-                    
-                if (this.showButtonCaption != '') {
+
+                if (this.showButtonCaption !== '') {
                     if (this.showImage) {
                         this._captionShow += '&nbsp;';
                     }
                     this._captionShow += this.showButtonCaption;
                 }
-                
-                if (this.hideButtonCaption != '') {
+
+                if (this.hideButtonCaption !== '') {
                     if (this.hideImage) {
                         this._captionHide += '&nbsp;';
                     }
                     this._captionHide += this.hideButtonCaption;
                 }
-                
-                templateWithView =      '<div class="input-group">' +
-                                            '    <input type="password" class="form-control password" id="' + this.id + '_password" />' +
-                                            '    <div class="input-group-addon" id="' + this.id + '_view">' + this._captionShow + '</div>' +
-                                            '</div>';
-                templateWithoutView =   '<input type="password" class="form-control password" id="' + this.id + '_password" />';
-                
+
+                templateWithView = '<div class="input-group">' +
+                    '    <input type="password" class="form-control password" id="' + this.id + '_password" />' +
+                    '    <div class="input-group-addon" id="' + this.id + '_view">' + this._captionShow + '</div>' +
+                    '</div>';
+                templateWithoutView = '<input type="password" class="form-control password" id="' + this.id + '_password" />';
+
                 //Setup controls
                 this._userInput = this.usernameInput;
                 attr.set(this._userInput, 'placeholder', this.userexample);
-                
-                if(this.showPasswordView === false){
+
+                if (this.showPasswordView === false) {
                     this.passwordContainer.innerHTML = templateWithoutView;
                 } else {
                     this.passwordContainer.innerHTML = templateWithView;
-                    this.connect(dom.byId(this.id + '_view'), 'click', dojo.hitch(this, function(){
+                    this.connect(dom.byId(this.id + '_view'), 'click', dojo.hitch(this, function () {
                         if (attr.get(this._passInput, 'type') === 'password') {
                             attr.set(this._passInput, 'type', 'text');
                             dom.byId(this.id + '_view').innerHTML = this._captionHide;
@@ -149,35 +149,35 @@
                     }));
                 }
                 this._passInput = dom.byId(this.id + '_password');
-                
+
                 attr.set(this._passInput, 'placeholder', this.passexample);
-                
-                if(this.autoCorrect){
+
+                if (this.autoCorrect) {
                     attr.set(this._userInput, 'autocorrect', 'on');
                 }
-                if(this.autoCapitalize){
+                if (this.autoCapitalize) {
                     attr.set(this._userInput, 'autocapitalize', 'on');
                 }
 
-                
+
                 // Setup text input elements
                 this.submitButton.value = this.logintext;
-		
+
                 if (this.forgotmf) {
                     this.forgotLink.innerHTML = this.forgottext;
                 } else {
                     domStyle.set(this.forgotPane, 'display', 'none');
                 }
-                
+
                 domStyle.set(this.messageNode, 'display', 'none');
-                
+
                 this._getI18NMap();
 
                 if (this.showprogress) {
                     this._indicator = mx.ui.getProgressIndicator('modal', this.progresstext);
-                }		
-		
-                if (typeof this.dofocus !== 'undefined' && this.dofocus) { 
+                }
+
+                if (typeof this.dofocus !== 'undefined' && this.dofocus) {
                     this._focusNode();
                 }
 
@@ -187,82 +187,101 @@
             // Attach events to newly created nodes.
             _setupEvents: function () {
 
-                on(this.submitButton, 'click', lang.hitch(this, function(e) {
-                    
+                on(this.submitButton, 'click', lang.hitch(this, function (e) {
+
                     var user = null,
                         pass = null,
                         promise = null;
-                    
+
                     domStyle.set(this.messageNode, 'display', 'none');
-                    
+
                     if (attr.get(this._passInput, 'type') === 'text') {
                         attr.set(this._passInput, 'type', 'password');
                         dom.byId(this.id + '_view').innerHTML = this._captionShow;
                     }
-                    
+
                     logger.debug(this.id + '.submitForm');
 
                     user = this._userInput.value;
                     pass = this._passInput.value;
 
-                    if(user && pass) {
-                        if (typeof this._indicator !== 'undefined' && this._indicator){
+                    if (user && pass) {
+                        if (typeof this._indicator !== 'undefined' && this._indicator) {
                             this._indicator.start();
                         }
 
-                        dojo.rawXhrPost({
-                            url			: mx.baseUrl,
-                            mimetype	: 'application/json',
-                            contentType	: 'application/json',
-                            handleAs	: 'json',
-                            headers     : {
-                                'csrfToken' : mx.session.getCSRFToken()
-                            },
-                            postData	: dojoJSON.toJson({
-                                action		: "login",
-                                params		: {
-                                    username	: user,
-                                    password	: pass,
-                                    locale		: ""
+                        if (this.loginmf) {
+                            var action = this.loginmf;
+                            var widget = this;
+                            var loginObj = null;
+
+                            mx.data.create({
+                                entity: this.loginEntity,
+                                callback: function (obj) {
+                                    loginObj = obj;
+                                    loginObj.set("Username", user);
+                                    loginObj.set("Password", pass);
+                                    mx.data.action({
+                                        params: {
+                                            actionname: action,
+                                            applyto: "selection",
+                                            guids: [obj.getGuid()]
+                                        },
+                                        callback: function (value) {
+                                            var localUser = loginObj.get("IsLocalUser");
+                                            if (localUser) {
+                                                widget._doLogin(user, pass);
+                                            } else {
+                                                widget._doLogin(user, "Mendix01");
+                                            }
+                                        },
+                                        error: function () {
+                                            console.error(widget.id + '.login: Error while calling microflow');
+                                        }
+                                    });
+                                },
+                                error: function (e) {
+                                    console.error('Error creating object: ' + e);
                                 }
-                            }),
-                            handle		: lang.hitch(this, this._validate)
-                        });
-                        
+                            });
+
+                        } else {
+                            this._doLogin(user, pass);
+                        }
+
                     } else {
                         domStyle.set(this.messageNode, 'display', 'block');
-                        this.messageNode.innerHTML = this.emptytext; 
+                        this.messageNode.innerHTML = this.emptytext;
                     }
 
                     event.stop(e);
 
                     return false;
 
-                })); 
-                
-                if(this.forgotmf)
-                {
-                    on(this.forgotLink, 'click', lang.hitch(this, function(e) {
+                }));
+
+                if (this.forgotmf) {
+                    on(this.forgotLink, 'click', lang.hitch(this, function (e) {
                         logger.debug(this.id + '.forgotPwd');
 
                         var action = this.forgotmf;
 
-                        if(action) {
+                        if (action) {
                             mx.data.action({
-                                params       : {
-                                    actionname : action
+                                params: {
+                                    actionname: action
                                 },
-                                callback	: function() {
+                                callback: function () {
                                     // ok	
                                 },
-                                error		: function() {
+                                error: function () {
                                     logger.error(this.id + '.forgotPwd: Error while calling microflow');
                                 }
                             });
                         }
 
                         event.stop(e);
-                    }));  
+                    }));
                 }
             },
 
@@ -273,13 +292,34 @@
              */
             _loadData: function () {
                 // TODO, get aditional data from mendix.
-            },            
-            
-            _validate : function(response, ioArgs) {
+            },
+
+            _doLogin: function (user, pass) {
+                dojo.rawXhrPost({
+                    url: mx.baseUrl,
+                    mimetype: 'application/json',
+                    contentType: 'application/json',
+                    handleAs: 'json',
+                    headers: {
+                        'csrfToken': mx.session.getCSRFToken()
+                    },
+                    postData: dojoJSON.toJson({
+                        action: "login",
+                        params: {
+                            username: user,
+                            password: pass,
+                            locale: ""
+                        }
+                    }),
+                    handle: lang.hitch(this, this._validate)
+                });
+            },
+
+            _validate: function (response, ioArgs) {
                 var i18nmap = null,
                     span = null,
                     message = ' ';
-                
+
                 logger.debug(this.id + '.validate');
 
                 if (typeof this._indicator !== 'undefined' && this._indicator) {
@@ -287,62 +327,71 @@
                 }
 
                 i18nmap = this._i18nmap;
-                
-                switch(ioArgs.xhr.status) {
-                    case 200 :
-                        mx.login();
-						return;
-                    case 400 :
-                    case 401 :
-                        message += i18nmap.http401;
-                        break;
-                    case 402 :
-                    case 403 :
-                        message += i18nmap.http401;
-                        break;
-                    case 404 :
-                        message += i18nmap.http404;
-                        break;
-                    case 500 :
-                        message += i18nmap.http500;
-                        break;
-                    case 503 :
-                        message += i18nmap.http503;
-                        break;
-                    default :
-                        message += i18nmap.httpdefault;
-                        break;
+
+                switch (ioArgs.xhr.status) {
+                case 200:
+                    mx.login();
+                    return;
+                case 400:
+                case 401:
+                    message += i18nmap.http401;
+                    break;
+                case 402:
+                case 403:
+                    message += i18nmap.http401;
+                    break;
+                case 404:
+                    message += i18nmap.http404;
+                    break;
+                case 500:
+                    message += i18nmap.http500;
+                    break;
+                case 503:
+                    message += i18nmap.http503;
+                    break;
+                default:
+                    message += i18nmap.httpdefault;
+                    break;
                 }
-				
+
                 this.messageNode.innerHTML = message; // is only reached when xhrstatus !== 200
                 domStyle.set(this.messageNode, 'display', 'block');
             },
 
-            _getI18NMap : function() {
+            _validateCustom: function () {
+                logger.debug(this.id + '.validateCustom');
+
+                if (typeof this._indicator !== 'undefined' && this._indicator) {
+                    this._indicator.stop();
+                }
+
+                this.messageNode.innerHTML = "The username or password you entered is incorrect.";
+                domStyle.set(this.messageNode, 'display', 'block');
+            },
+
+            _getI18NMap: function () {
                 logger.debug(this.id + '.injectI18NMap');
 
                 if (!window.i18n) {
                     dojo.xhrGet({
-                        url			: mx.appUrl + 'js/login_i18n.js',
-                        handleAs	: 'javascript',
-                        sync		: true
+                        url: mx.appUrl + 'js/login_i18n.js',
+                        handleAs: 'javascript',
+                        sync: true
                     });
                 }
 
                 this._i18nmap = window.i18nMap;
             },
 
-            _focusNode : function() {
+            _focusNode: function () {
                 logger.debug(this.id + '.focusNode');
 
-                setTimeout(lang.hitch(this, function() {
+                setTimeout(lang.hitch(this, function () {
                     this.usernameInput.focus();
                 }), 0);
             }
-            
+
         });
     });
 
 }());
-
-
